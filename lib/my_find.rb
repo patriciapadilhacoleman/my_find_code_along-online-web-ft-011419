c961d7ebcbab6a7f8578941bf1604b79633a1dc8
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-14 19:52:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,10 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
+    
+    return collection[i] if yield(collection[i])
     i = i + 1
-    if yield(collection[i])
-      return collection[i]
-    end
+    
   end
 
 end
